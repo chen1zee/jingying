9c8159849e466d0775ad2351c3881ce3cd06f22b
chen1zee <chenshaoh@163.com>
2016-07-27 09:10:42 +0800

    c-morning
    asdasdasd

--- js/conn.php
+++ js/conn.php
@@ -7,23 +7,7 @@
 	// 连接，返回资源标识符
 	$conn = @mysql_connect($url,$root,$pwd) or die("连接失败");
 
-
-
-
-	// 2，选择数据库
+	// 选择数据库
 	mysql_select_db('gzh160301');
 	mysql_query('set names utf8');
-
-
-	$sql = 'select * from student';
-
-	$result = mysql_query($sql);
-	
-	
-	while($row = mysql_fetch_array($result)){
-		echo '名字：'.$row['name'].'<br />';
-		echo '性别：'.$row['gender'].'<br />';
-	}
-
-
  ?>
